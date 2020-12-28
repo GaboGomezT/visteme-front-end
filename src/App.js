937459beda40b02a3b39c799f0b59e7bdb79f4e9
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-28 11:12:21 -0600

    Converted login into component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-
+import Login from './components/Login.js';
 
 function App() {
   return (
-    <div className="bg-indigo-500 h-screen flex items-center justify-center">
-      <div className="bg-white rounded-lg p-5 text-center shadow-2xl">
-        <h1 className="text-5xl mb-4 italic py-5">Vísteme</h1>
-        <form className="px-6">
-          <div>
-            <input type="text" placeholder="Correo" className="bg-indigo-200 py-2 my-2" />
-          </div>
-          <div>
-            <input type="password" placeholder="Contraseña" className="bg-indigo-200 py-2 my-2" />
-          </div>
-          <div>
-            <button type="submit" className="bg-indigo-500 rounded text-white px-3 py-2 mt-2">Iniciar Sesión</button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Login />
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="bg-indigo-500 h-screen flex items-center justify-center">
+            <div className="bg-white rounded-lg p-5 text-center shadow-2xl">
+                <h1 className="text-5xl mb-4 italic py-5">Vísteme</h1>
+                <form className="px-6">
+                    <div>
+                        <input type="email" placeholder="Correo" className="bg-indigo-200 p-2 my-2 rounded" />
+                    </div>
+                    <div>
+                        <input type="password" placeholder="Contraseña" className="bg-indigo-200 p-2 my-2 rounded" />
+                    </div>
+                    <div>
+                        <button type="submit" className="bg-indigo-500 rounded text-white px-3 py-2 mt-2">Iniciar Sesión</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
